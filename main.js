b4afe1790e03072358fced86b27afd2fe468db64
Paola Barreto <paola.valentina080670@gmail.com>
2022-07-19 17:43:03 -0500

    Parte 3 y 4

--- main.js
+++ main.js
@@ -22,14 +22,25 @@
         //get para obtener elemntos(barras)
         get elements() {
             let elements = this.bars;
-            elements.push(this.ball)
+//            elements.push(this.ball)
             return elements
         }
     }
 
 })();
 
-
+(function(){
+    self.Ball = function (x, y, radius, board){
+this.x = x;
+this.y = y;
+this.radius = radius
+this.speed_y =0;
+this.speed_x = 3;
+this.board = board;
+board.ball = this;
+this.kind = "circle"
+    }
+})();
 //Especie de constructor para la barra
 (function(){
 self.Bar = function(x, y, width, height, board){
@@ -43,17 +54,19 @@ self.Bar = function(x, y, width, height, board){
 
  //definimos la forma
     this.kind = "rectangle"
-
-    
+    this.speed  = 10
 }
 
 //declaramos funciones para moverlo
  self.Bar.prototype = {
- down: function(){
-
+ down: function(){ 
+    this.y += this.speed
  },
  up : function(){
-
+this.y -= this.speed;
+ },
+ toString:  function(){
+    return "x: " + this.x + "y: " + this.y;
  }
 }
 })();
@@ -61,7 +74,7 @@ self.Bar = function(x, y, width, height, board){
 
 //función para la vista del board
 (function () {
-    self.BoardView = function (canvas, board) {
+    self.BoardView = function(canvas, board) {
         this.canvas = canvas;
         this.canvas.width = board.width;
         this.canvas.height = board.height;
@@ -73,13 +86,20 @@ self.Bar = function(x, y, width, height, board){
 
 //dibujo con elemento a dibujar
     self.BoardView.prototype = {
+
+       
         draw: function(){
             for (var i = this.board.elements.length - 1; i>=0; i--){
               let el =  this.board.elements[i];
 
               draw(this.ctx, el)
             }
+        },
+        play: function(){
+            this.clean();
+            this.draw()
         }
+    
     }
     //para el dibujo
     function draw(ctx, element){
@@ -88,22 +108,49 @@ self.Bar = function(x, y, width, height, board){
    switch(element.kind){
   case "rectangle":
     console.log(element)
-    ctx.fillRect(element.x, element.y,  element.height, element.width,)
+    ctx.fillRect(element.x, element.y,  element.height, element.width)
+   case  "circle":
+    ctx.beginPath();
+    ctx.arc(element.x, element.y, element.radius, 0,7)
+    ctx.fill();
+    ctx.closePath()
     break;
 }
     }
 
 })();
 
+let board = new Board(800, 400);
+let bar = new Bar(20, 100, 40,  100, board)
+let bar_2 = new Bar(735, 100, 40,  100, board)
+let canvas = document.getElementById("canvas")
+let board_view = new BoardView(canvas, board);
+let ball = new Ball(350, 100, 10, board)
+window.requestAnimationFrame(main);
+function controller(){
+    board_view.play();
+    board_view.draw();
+    window.requestAnimationFrame(controller);
+}
 
+document.addEventListener("keydown", function(ev){
+    ev.preventDefault();
+console.log(ev.keyCode);
+
+if(keyCode == 38){
+    bar.up();
+}else if (ev.keyCode == 40){
+    bar.down()
+}else if(ev.keyCode == 87){
+  bar_2.up();
+}else if(ev.keyCode == 83){
+    bar_2.down()
+}
+console.log(" "  +bar)
+})
 self.addEventListener("load", main)
 // Controlador para instanciar los objetos y pasarle modelo a la vista
 function main() {
-    let board = new Board(1600, 400);
-    let bar = new Bar(20, 100, 40,  100, board)
-    let canvas = document.getElementById("canvas")
-    let board_view = new BoardView(canvas, board);
-
-
     board_view.draw()
+
 }
